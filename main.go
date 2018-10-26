271a37f91ae51c5736939575623b8dd3bc45c804
Leon <yanglyu.leon.7@gmail.com>
2018-10-25 20:43:10 -0700

    demo: Adding functionalities

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -13,13 +14,13 @@ func main() {
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
 
 	// To make sure that the API is up and running
-	r.Handle("/status", NotImplemented).Methods("GET")
+	r.Handle("/status", StatusHandler).Methods("GET")
 
 	// To retrieve a list of products that the user can leave feedback on
-	r.Handle("/products", NotImplemented).Methods("GET")
+	r.Handle("/products", ProductsHandler).Methods("GET")
 
 	// To capture user feedback on products
-	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+	r.Handle("/products/{slug}/feedback", AddFeedbackHandler).Methods("POST")
 
 	// To setup the server so one can serve static assets like images, css
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("/static"))))
@@ -30,4 +31,52 @@ func main() {
 
 var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not Implemented"))
+})
+
+type Product struct {
+	Id          int
+	Name        string
+	Slug        string
+	Description string
+}
+
+var products = []Product{
+	Product{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters", Description: "Shoot your way to the top on 14 different hoverboards"},
+	Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
+	Product{Id: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
+	Product{Id: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+	Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
+	Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
+}
+
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API is up and running"))
+})
+
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// To convert the slice of products to JSON
+	payload, _ := json.Marshal(products)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+})
+
+var AddFeedbackHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	var product Product
+	vars := mux.Vars(r)
+	slug := vars["slug"]
+
+	for _, p := range products {
+		if p.Slug == slug {
+			product = p
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if product.Slug != "" {
+		payload, _ := json.Marshal(product)
+		w.Write([]byte(payload))
+	} else {
+		w.Write([]byte("Product Not Found"))
+	}
 })
